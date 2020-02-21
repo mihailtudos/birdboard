148897e4f14d9b68aab648d92aef54e0be526fd0
mihailtudos <mihailtudos@outlook.com>
2020-02-21 20:31:45 +0000

    imported vue and vue-js-modal, built the form to create new projects

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,26 @@
+require("./bootstrap");
 /**
- * First, we will load all of this project's Javascript utilities and other
- * dependencies. Then, we will be ready to develop a robust and powerful
- * application frontend using useful Laravel and JavaScript libraries.
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+import Vue from 'vue';
+
+
+Vue.use(VModal);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+window.events = new Vue();
+
+import VModal from 'vue-js-modal';
+Vue.use(VModal);
+
+const app = new Vue({
+    el: '#app'
+});
