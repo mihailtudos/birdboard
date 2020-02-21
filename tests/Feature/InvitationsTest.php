bd21d575d1e751078acb4954cd13abae7d5f7ce3
mihailtudos <mihailtudos@outlook.com>
2020-02-21 01:35:27 +0000

    invite users feature tested through TDD

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -14,8 +14,49 @@ class InvitationsTest extends TestCase
     use RefreshDatabase;
 
     /** @test*/
+    public function only_the_owners_of__the_projects_can_invite_members()
+    {
+        //if you are not the owner of the project you cannot add other members
+        $this->actingAs(factory(User::class)->create())
+            ->post(ProjectFactory::create()->path() .'/invitations')
+            ->assertStatus(403);
+    }
+
+    /** @test*/
 
     public function project_owner_can_invite_users()
+    {
+
+        $project = ProjectFactory::create();
+
+        $invitedUser = factory(User::class)->create();
+
+        $this->actingAs($project->owner)
+            ->post( $project->path() . '/invitations', [
+            'email' => $invitedUser->email])
+            ->assertRedirect($project->path());
+
+        $this->assertTrue($project->members->contains($invitedUser));
+    }
+
+    /** @test*/
+    public function the_invited_user_must_have_a_valid_account()
+    {
+        //given we have a project
+        $project = ProjectFactory::create();
+
+        //sign in as the owner of the project
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+            'email' => 'notauser@example.com'
+        ])->assertSessionHasErrors([
+            'email' => 'The invited user does not have an account'
+            ]);
+    }
+
+    /** @test*/
+
+    public function project_members_can_update_project_details()
     {
         //given we have a project
         //the owner of the project invites another user
